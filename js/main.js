0bff4a17a889d956d7415882e5334a1288f72947
Dario <dario.defrancisci92@gmail.com>
2021-09-06 13:41:23 +0200

    Esercizio terminato

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
+// Vue js
+
+Vue.config.devtools = true; // scrivere per attivare l'estenzione sul browser
+
 //Consegna:
 //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 
+const title = new Vue (
+    {
+        el: '#root',
+        data: {
+            message: '  Example title'
+        }
+    }
+)
 
 
 //Bonus:
